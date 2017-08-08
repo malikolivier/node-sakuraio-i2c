ef0d9649df3a5a8858b46ef59dde3280093a8ba9
Malik Olivier Boussejra <malik@boussejra.com>
2017-08-08 22:32:30 +0900

    I2c: Implement functional I2c sync write/read

--- sakuraio-i2c.js
+++ sakuraio-i2c.js
@@ -4,19 +4,58 @@ const SakuraIO = require('sakuraio')
 const SAKURAIO_SLAVE_ADDR = 0x4F
 
 function decorateI2c (bus) {
+  var i = 0
+  var request = Buffer.alloc(32)
+  var response = Buffer.alloc(32)
   return SakuraIO.use({
+    startWrite (cb) {
+      i = 0
+      cb()
+    },
+    startWriteSync () {
+      i = 0
+    },
+    endWrite (cb) {
+      // TODO
+      cb()
+    },
+    endWriteSync () {
+      var buf = Buffer.alloc(request[1] + 2)
+      request.copy(buf, 0, 1)
+      console.log('->', buf)
+      bus.writeI2cBlockSync(SAKURAIO_SLAVE_ADDR, request[0], buf.length, buf)
+    },
     sendByte (byte, cb) {
-      bus.sendByte(SAKURAIO_SLAVE_ADDR, byte, cb)
+      i += 1
+      request[i] = byte
+      cb()
     },
     sendByteSync (byte) {
-      bus.sendByteSync(SAKURAIO_SLAVE_ADDR, byte)
+      i += 1
+      request[i] = byte
     },
 
+    startRead (cb) {
+      i = 0
+      cb()
+    },
+    startReadSync () {
+      i = 0
+      bus.readI2cBlockSync(SAKURAIO_SLAVE_ADDR, 32, 32, response)
+      console.log('<-', response)
+    },
+    endRead (cb) {
+      // TODO
+      cb()
+    },
+    endReadSync () {},
     receiveByte (cb) {
-      bus.receiveByte(SAKURAIO_SLAVE_ADDR, cb)
+      i += 1
+      cb(null, response[i-1])
     },
     receiveByteSync () {
-      return bus.receiveByteSync(SAKURAIO_SLAVE_ADDR)
+      i += 1
+      return response[i-1]
     }
   })
 }
